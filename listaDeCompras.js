244fab412ab4a3c6a0660e816a66b763a62e6a97
andre_barbosa <oliveiraandre945@gmail.com>
2023-08-14 21:47:26 -0300

    arrumei o alert da lista de compras

--- listaDeCompras.js
+++ listaDeCompras.js
@@ -12,32 +12,31 @@ let listaDeCompras = {
 while (true) {
     let desejo = prompt("Deseja colocar alguma coisa no seu carrinho? Sim ou Não?");
 
-        if (desejo == "Sim") {
-                let comidaInserida = prompt("Qual comida você deseja inserir?");
-                let qualCategoria = prompt('Em qual categoria ela se encaixa: frutas, laticínios, congelados, doces.');
+    if (desejo == "sim") {
+        let comidaInserida = prompt("Qual comida você deseja inserir?");
+        let qualCategoria = prompt('Em qual categoria ela se encaixa: frutas, laticínios, congelados, doces.');
 
-             if (qualCategoria == "frutas") {
-                listaDeCompras.frutas.push(comidaInserida);
-            } else if (qualCategoria == "laticínios") {
-                listaDeCompras.laticinios.push(comidaInserida);
-            } else if (qualCategoria == "congelados") {
-                listaDeCompras.congelados.push(comidaInserida);
-            } else if (qualCategoria == "doces") {
-                listaDeCompras.doces.push(comidaInserida);
-            } else {
-                alert("Categoria Inválida");
-            }
-        } else if (desejo == Não) {
-            
-           let listaFormatada = `Na sua lista foi adicionado:\n
-           Frutas: ${formatarLista(listaDeCompras.frutas)}\n
-           Laticínios: ${formatarLista(listaDeCompras.laticinios)}\n
-           Congelados: ${formatarLista(listaDeCompras.congelados)}\n
-           Doces: ${formatarLista(listaDeCompras.doces)}`;
-
-            alert(listaFormatada);
-            break;
+        if (qualCategoria == "frutas") {
+            listaDeCompras.frutas.push(comidaInserida);
+        } else if (qualCategoria == "laticínios") {
+            listaDeCompras.laticinios.push(comidaInserida);
+        } else if (qualCategoria == "congelados") {
+            listaDeCompras.congelados.push(comidaInserida);
+        } else if (qualCategoria == "doces") {
+            listaDeCompras.doces.push(comidaInserida);
         } else {
-            alert("Resposta inválida. Por favor, responda 'Sim' ou 'Não'.");
+            alert("Categoria Inválida");
         }
+    } else if (desejo == "não") {
+        let listaFormatadaText = `Frutas: ${listaFormatada(listaDeCompras.frutas)}\n` +
+                                 `Laticínios: ${listaFormatada(listaDeCompras.laticinios)}\n` +
+                                 `Doces: ${listaFormatada(listaDeCompras.doces)}\n` +
+                                 `Congelados: ${listaFormatada(listaDeCompras.congelados)}`;
+    
+        let listaCompleta = `Lista de Compras:\n${listaFormatadaText}`;
+        alert(listaCompleta);
+        break;
+    } else {
+        alert("Resposta inválida. Por favor, responda 'Sim' ou 'Não'.");
+    }
 }
